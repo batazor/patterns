9412c957b8bbc31574e4c41c4f7ef827f2a1ee2d
Victor Login <batazor111@gmail.com>
2019-07-03 01:33:40 +0300

    Add singleton

---
+++ creational/singleton/singleton_test.go
@@ -0,0 +1,30 @@
+package singleton
+
+import "testing"
+
+func TestGetInstance(t *testing.T) {
+  counter1 := GetInstance()
+
+  if counter1 == nil {
+    // Test of acceptance criteria 1 failed
+    t.Error("expected pointer to Singleton after calling GetInstance(), not nil")
+  }
+
+  expectedCounter := counter1
+
+  currentCount := counter1.AddOne()
+  if currentCount != 1 {
+    t.Errorf("After calling the first time to count, the count must be 1 but it is %d\n", currentCount)
+  }
+
+  counter2 := GetInstance()
+  if counter2 != expectedCounter {
+    // Test 2 failed
+    t.Errorf("Expected same instance in counter2 but it got a different instance")
+  }
+
+  currentCount = counter2.AddOne()
+  if currentCount != 2 {
+    t.Errorf("After calling 'AddOne' using the second counter, the current count must be 2 but was %d/n", currentCount)
+  }
+}
